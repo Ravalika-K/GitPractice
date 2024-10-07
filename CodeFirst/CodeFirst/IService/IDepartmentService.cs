c9b8027c8ac215320833eaac77024b7f6d1281d6
Ravalika-K <ravalikakondapuram@gmail.com>
2024-10-07 18:26:18 +0530

    added

---
+++ CodeFirst/CodeFirst/IService/IDepartmentService.cs
@@ -0,0 +1,13 @@
+﻿using CodeFirst.Models;
+
+namespace CodeFirst.IService
+{
+    public interface IDepartmentService
+    {
+        List<Department> GetAllDepartments();
+        Department AddNewDepartment(Department department);
+        string DeleteDepartment(int id);
+        string UpdateDepartment(Department department);
+        Department GetDepartmentById(int id);
+    }
+}
